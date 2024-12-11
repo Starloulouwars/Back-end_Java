dfa7c75c2a3e4f1608714fb5f384917b173e1cdb
LukeTHEVARD <luke.thevard@stagiairesmns.fr>
2024-12-11 16:09:29 +0100

    11/12/2024 (test unitaire)

---
+++ src/main/java/com/example/demo/dto/PrioriteTacheDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PrioriteTacheDto {
+
+    public String name;
+    private long numberUser;
+}
